2dbcda0d475b1f54f1b2091a939e2d99ebc75d42
Tran Cong Dung <dung.tran21@pn.com.vn>
2020-06-15 08:27:22 +0700

    cau 1 va 2

---
+++ database/seeds/TourSeeder.php
@@ -0,0 +1,18 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class TourSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        for($i=0;$i<10;$i++){
+
+        }
+    }
+}
